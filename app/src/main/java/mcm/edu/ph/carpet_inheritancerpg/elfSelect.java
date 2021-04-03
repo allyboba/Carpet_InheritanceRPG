9b0d4c34737d3e9243cac9f50cbd0a883366429f
allyboba <ally.martine0807@gmail.com>
2021-04-03 17:15:28 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/carpet_inheritancerpg/elfSelect.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.carpet_inheritancerpg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class elfSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_elf_select);
+    }
+}
---
+++ app/src/main/java/mcm/edu/ph/carpet_inheritancerpg/orcInfo.java
@@ -0,0 +1,14 @@
+package mcm.edu.ph.carpet_inheritancerpg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class orcInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orc_info);
+    }
+}
